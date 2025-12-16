6d7e5cd699dbcda19e166cff26644680552714a8
ctgoyette <ctgoyette21@gmail.com>
2025-12-16 15:50:18 -0500

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,13 @@
 #include "robot_config.hpp"
 
 /**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
-/**
  * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	robotInit();
 }
 
 /**
@@ -78,13 +59,7 @@ void autonomous() {}
 void opcontrol() {
 	opcontrolInit();
 
-	controller.initialize();
-
 	while (true) {
-		if (!controller.ButtonB.isPressed()) {
-			intake.stop();
-		}
-
 		pros::delay(20);
 	}
 }
--- src/robot_config.cpp
+++ src/robot_config.cpp
@@ -1,12 +1,13 @@
 #include "robot_config.hpp"
 
 #define ROBOT_1
+// #define ROBOT_2
 
 HskyController controller(pros::E_CONTROLLER_MASTER);
 
-//---------------------------------------------------
+//----------------------------------------------------
 // ##################### Robot 1 #####################
-//---------------------------------------------------
+//----------------------------------------------------
 
 #ifdef ROBOT_1
 
@@ -45,76 +46,17 @@ pros::MotorGroup intakeMotorGroup({5});
 Transport intake(intakeMotorGroup, 0.05, pros::E_MOTOR_BRAKE_COAST,
 				 pros::E_MOTOR_GEAR_600);
 
-//---------------------------------------------------
+//----------------------------------------------------
 // ##################### Robot 2 #####################
-//---------------------------------------------------
+//----------------------------------------------------
 
 #elifdef ROBOT_2
 
 //===================== CONFIG =====================
 
-PIDController<Pose>::pid_config_t drivePIDConfig{.kP = 0.0,
-												 .kI = 0.0,
-												 .kD = 0.0,
-
-												 .deadband = 0.0};
-PIDController<double>::pid_config_t turnPIDConfig{.kP = 0.0,
-												  .kI = 0.0,
-												  .kD = 0.0,
-
-												  .deadband = 0.0};
-
-PIDController<Pose> drivePID(drivePIDConfig);
-PIDController<double> turnPID(turnPIDConfig);
-
-robot_specs_t robotConfig{.driveWheelDiam = 0.0,
-						  .trackWidth = 0.0,
-						  .odomWheelDiam = 0.0,
-
-						  .maxDrivePct = 0,
-						  .maxTurnPct = 0,
-
-						  .drivePID = &drivePID,
-						  .turnPID = &turnPID};
-
 //===================== DEVICES =====================
 
-pros::MotorGroup leftMotorGroup({-10, -1, 18});
-pros::MotorGroup rightMotorGroup({4, 5, -2});
-
-pros::MotorGroup intakeMotorGroup({-16, 6});
-
-pros::MotorGroup scoringMotorGroup({9, -14});
-
-pros::adi::DigitalOut mogoClampCylinder('a');
-
-pros::Rotation xEncoder(5);
-pros::Rotation yEncoder(-10);
-
-pros::IMU imu1(3);
-pros::IMU imu2(7);
-
 //==================== SUBSYSTEMS ====================
-
-PIDController<Pose> drivePIDMove(1.0, 0.0, 0.1);
-PIDController<double> drivePIDAngular(2, 0.05, 0.1);
-
-OdometryPerpendicularIMU odom(&xEncoder, &yEncoder, &imu1, &imu2);
-
-TankDrive tankdrive(ctrl, leftMotorGroup, rightMotorGroup, DriveStyle::ARCADE,
-					pros::E_MOTOR_BRAKE_COAST, pros::E_MOTOR_GEAR_600, 0.75,
-					&odom, &drivePIDMove, &drivePIDAngular);
-
-Transport intake(ctrl, intakeMotorGroup, 1.0, pros::E_CONTROLLER_DIGITAL_L1,
-				 pros::E_CONTROLLER_DIGITAL_L2, pros::E_MOTOR_BRAKE_COAST,
-				 pros::E_MOTOR_GEAR_600);
-
-Transport scoring(ctrl, scoringMotorGroup, 0.25, pros::E_CONTROLLER_DIGITAL_R1,
-				  pros::E_CONTROLLER_DIGITAL_R2, pros::E_MOTOR_BRAKE_COAST,
-				  pros::E_MOTOR_GEAR_600);
-
-Pneumatics mogoClamp(ctrl, mogoClampCylinder, pros::E_CONTROLLER_DIGITAL_B,
-					 pros::E_CONTROLLER_DIGITAL_X);
 
 #endif
 
@@ -122,17 +64,11 @@ Pneumatics mogoClamp(ctrl, mogoClampCylinder, pros::E_CONTROLLER_DIGITAL_B,
 
 void deviceInit() {}
 
+void robotInit() { deviceInit(); }
+
 void opcontrolInit() {
+	controller.ButtonB.onHold([]() { intake.moveOut(); });
+	controller.ButtonB.onReleased([]() { intake.stop(); });
 
-	controller.ButtonB.onHold([] (){
-		intake.moveOut();
-	});
-
-	controller.ButtonLeft.onPressed([]() {
-		pros::lcd::set_text(1, "Left Button");
-	});
-}
-
-void robotInit() {
-	deviceInit();
+	controller.initialize();
 }
