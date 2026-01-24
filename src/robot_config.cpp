3f6bb8a2ea802b9fae4a0bb78911979ad5170a0d
Casey <ctgoyette21@gmail.com>
2026-01-24 01:01:01 -0500

    Controller changes

--- src/robot_config.cpp
+++ src/robot_config.cpp
@@ -8,15 +8,13 @@
 #include "pros/motors.h"
 
 HskyController controller(pros::E_CONTROLLER_MASTER);
-std::queue<Command*> commandQueue;
+std::queue<Command *> commandQueue;
 
 // ---------------------------------------------------------
 // ##################### Configuration #####################
 // ---------------------------------------------------------
 #define ROBOT_2
 #define BLUE
-
-
 
 //---------------------------------------------------
 // ##################### Robot 1 #####################
@@ -48,6 +46,8 @@ pros::adi::DigitalOut scraperCylinder('c');
 pros::adi::DigitalOut hoodCylinder('a');
 pros::adi::DigitalOut wingCylinder('b');
 
+pros::Optical opticalSensor(15);
+
 // //==================== SUBSYSTEMS ====================
 // TankDrive driveBase(leftDriveMotors, rightDriveMotors, DriveStyle::ARCADE,
 
@@ -74,14 +74,13 @@ Pneumatics wing(wingCylinder);
 //---------------------------------------------------
 
 #elifdef ROBOT_2
-bool isLeft = true; // TODO: Should be true
+bool isLeft = true;	 // TODO: Should be true
 
 //===================== CONFIG =====================
 
 PIDController drivePid(0.1, 0.0, 0, PIDController::ERROR_TYPE::LINEAR);
 // PIDController drivePid(0.15, 0.0, 0.05, PIDController::ERROR_TYPE::LINEAR);
 PIDController turnPid(53.0, 0.67, 115.0, PIDController::ERROR_TYPE::ANGULAR);
-
 
 robot_specs_t robotConfig{.driveWheelDiam = 0.0,
 						  .trackWidth = 11.0,
@@ -106,14 +105,14 @@ pros::adi::DigitalOut scraperCylinder('h');
 pros::adi::DigitalOut hoodCylinder('g');
 pros::adi::DigitalOut wingCylinder('a');
 
-DrivebaseIMUOdometry odom(&leftDriveMotors, &rightDriveMotors,
-						&imu, pros::E_MOTOR_GEAR_600, robotConfig.trackWidth);
+DrivebaseIMUOdometry odom(&leftDriveMotors, &rightDriveMotors, &imu,
+						  pros::E_MOTOR_GEAR_600, robotConfig.trackWidth);
 
 //==================== SUBSYSTEMS ====================
 
 TankDrive driveBase(leftDriveMotors, rightDriveMotors, DriveStyle::ARCADE,
 					pros::E_MOTOR_BRAKE_COAST, pros::E_MOTOR_GEAR_600, 1.0,
-				robotConfig, &odom);
+					robotConfig, &odom);
 Transport lowerScoring(lowerScoringMotors, 1, pros::E_MOTOR_BRAKE_COAST,
 					   pros::E_MOTOR_GEAR_600);
 Transport upperScoring(upperScoringMotors, 1, pros::E_MOTOR_BRAKE_COAST,
@@ -138,8 +137,8 @@ bool isRed = false;
 
 //====================== UTILS ======================
 
-void deviceInit() {	
-	pros::delay(1000);  // Allow time for devices to initialize
+void deviceInit() {
+	pros::delay(1000);	// Allow time for devices to initialize
 	odom.reset();
 	odom.init();
 
@@ -147,7 +146,6 @@ void deviceInit() {
 	while (imu.is_calibrating() || !std::isfinite(imu.get_heading())) {
 		pros::delay(20);
 	}
-
 }
 void scoreLong() {
 	hood.extendPiston();
@@ -201,30 +199,24 @@ void stopAll() {
 void constructAuton(bool isLeft, bool isRed) {
 	// Drive to loader
 	commandQueue.push(new DriveDistance(driveBase, odom, 40.0, 1500));
-	commandQueue.push(new TurnToHeading(driveBase, odom, isLeft ? 90.0: 270.0));
+	commandQueue.push(
+		new TurnToHeading(driveBase, odom, isLeft ? 90.0 : 270.0));
 
 	// Intake from loader
-	commandQueue.push(new InstantCommand([&]() {
-		intakeLoader();
-	}));
+	commandQueue.push(new InstantCommand([&]() { intakeLoader(); }));
 	commandQueue.push(new DriveDistance(driveBase, odom, 18, 1400));
 	commandQueue.push(new TimeoutCommand(2000));
-	commandQueue.push(new InstantCommand([&]() {
-		stopAll();
-	}));
-	
+	commandQueue.push(new InstantCommand([&]() { stopAll(); }));
+
 	// Back up and turn to corner
 	commandQueue.push(new DriveDistance(driveBase, odom, -22, 1500));
-	commandQueue.push(new TurnToHeading(driveBase, odom, isLeft ? 45.0 : 315.0));
+	commandQueue.push(
+		new TurnToHeading(driveBase, odom, isLeft ? 45.0 : 315.0));
 
 	// Spit out wrong color
-	commandQueue.push(new InstantCommand([&]() {
-		scoreLower();
-	}));
+	commandQueue.push(new InstantCommand([&]() { scoreLower(); }));
 	commandQueue.push(new WaitUntilColorSensor(opticalSensor, isRed, 2000));
-	commandQueue.push(new InstantCommand([&]() {
-		stopAll();
-	}));
+	commandQueue.push(new InstantCommand([&]() { stopAll(); }));
 
 	// // Spit out again to avoid getting stuck
 	// commandQueue.push(new InstantCommand([&]() {
@@ -240,42 +232,34 @@ void constructAuton(bool isLeft, bool isRed) {
 	// }));
 
 	// Drive to goal
-	commandQueue.push(new TurnToHeading(driveBase, odom, isLeft ? 90.0 : 270.0));
+	commandQueue.push(
+		new TurnToHeading(driveBase, odom, isLeft ? 90.0 : 270.0));
 	commandQueue.push(new DriveDistance(driveBase, odom, -18, 1500));
 
 	// Score
-	commandQueue.push(new InstantCommand([&]() {
-		scoreLong();
-	}));
+	commandQueue.push(new InstantCommand([&]() { scoreLong(); }));
 	commandQueue.push(new TimeoutCommand(1000));
 	commandQueue.push(new DriveDistance(driveBase, odom, -7, 1000));
 	commandQueue.push(new TimeoutCommand(1000));
-	commandQueue.push(new InstantCommand([&]() {
-		stopAll();
-	}));
+	commandQueue.push(new InstantCommand([&]() { stopAll(); }));
 
 	// Drive to loader again
-	commandQueue.push(new InstantCommand([&]() {
-		intakeLoader();
-	}));
+	commandQueue.push(new InstantCommand([&]() { intakeLoader(); }));
 	commandQueue.push(new DriveDistance(driveBase, odom, 18, 1500));
 	commandQueue.push(new TimeoutCommand(100));
 	commandQueue.push(new DriveDistance(driveBase, odom, 16, 1500));
 
 	// Intake
 	commandQueue.push(new TimeoutCommand(2000));
-	commandQueue.push(new InstantCommand([&]() {
-		stopAll();
-	}));
+	commandQueue.push(new InstantCommand([&]() { stopAll(); }));
 
 	// Go to goal
-	commandQueue.push(new TurnToHeading(driveBase, odom, isLeft ? 90.0 : 270.0));
+	commandQueue.push(
+		new TurnToHeading(driveBase, odom, isLeft ? 90.0 : 270.0));
 	commandQueue.push(new DriveDistance(driveBase, odom, -37, 1500));
 
 	// Score
-	commandQueue.push(new InstantCommand([&]() {
-		scoreLong();
-	}));
+	commandQueue.push(new InstantCommand([&]() { scoreLong(); }));
 	commandQueue.push(new TimeoutCommand(5000));
 
 	// Stop and flip hood
@@ -291,33 +275,101 @@ void constructAuton(bool isLeft, bool isRed) {
 	commandQueue.push(new TimeoutCommand(100000));
 }
 
+#ifdef ROBOT_1
+bool scraperToggle = false;
+bool wingToggle = false;
+
 void opcontrolInit() {
-	controller.ButtonR1.onHold([]() { scoreUpper(); });
+	// Intake
+	controller.ButtonR1.onPressed([]() { intakeField(); });
 	controller.ButtonR1.onReleased([]() { stopAll(); });
 
+	// Score Long
 	controller.ButtonR2.onPressed([]() { scoreLong(); });
 	controller.ButtonR2.onReleased([]() { stopAll(); });
 
+	// Hood
+	controller.ButtonY.onPressed([]() { hood.extendPiston(); });
+	controller.ButtonY.onReleased([]() { hood.retractPiston(); });
+
+	// Score Lower
+	controller.ButtonL1.onPressed([]() { scoreLower(); });
+	controller.ButtonL1.onReleased([]() { stopAll(); });
+
+	// Score Upper
+	controller.ButtonL2.onPressed([]() { scoreUpper(); });
+	controller.ButtonL2.onReleased([]() { stopAll(); });
+
+	// Upper Scoring Backward
+	controller.ButtonLeft.onPressed([]() { upperScoring.moveIn(); });
+	controller.ButtonLeft.onReleased([]() { stopAll(); });
+
+	// Scraper
+	controller.ButtonA.onPressed([]() {
+		scraperToggle = !scraperToggle;
+		if (scraperToggle) {
+			scraper.extendPiston();
+		} else {
+			scraper.retractPiston();
+		}
+	});
+
+	// Wing
+	controller.ButtonRight.onPressed([]() {
+		wingToggle = !wingToggle;
+		if (wingToggle) {
+			wing.extendPiston();
+		} else {
+			wing.retractPiston();
+		}
+	});
+}
+
+#elifdef ROBOT_2
+bool wingToggle = false;
+
+void opcontrolInit() {
+	// Score Upper
+	controller.ButtonR1.onHold([]() { scoreUpper(); });
+	controller.ButtonR1.onReleased([]() { stopAll(); });
+
+	// Score Long
+	controller.ButtonR2.onPressed([]() { scoreLong(); });
+	controller.ButtonR2.onReleased([]() { stopAll(); });
+
+	// Intake
 	controller.ButtonL1.onPressed([]() { intakeField(); });
 	controller.ButtonL1.onReleased([]() { stopAll(); });
 
+	// Intake
 	controller.ButtonL2.onPressed([]() { intakeLoader(); });
 	controller.ButtonL2.onReleased([]() {
 		stopAll();
 		scraper.retractPiston();
 	});
 
-	controller.ButtonY.onPressed([]() { wing.extendPiston(); });
-	controller.ButtonY.onReleased([]() { wing.retractPiston(); });
+	// Score Lower
+	controller.ButtonY.onPressed([]() { scoreLower(); });
+	controller.ButtonY.onReleased([]() { stopAll(); });
 
-	controller.ButtonA.onPressed([]() { scoreLower(); });
-	controller.ButtonA.onReleased([]() { stopAll(); });
+	// Hood
+	controller.ButtonX.onPressed([]() { hood.extendPiston(); });
+	controller.ButtonX.onReleased([]() { hood.retractPiston(); });
 
-	controller.ButtonB.onPressed([]() { hood.extendPiston(); });
-	controller.ButtonB.onReleased([]() { hood.retractPiston(); });
+	// Wing
+	controller.ButtonRight.onPressed([]() {
+		wingToggle = !wingToggle;
+		if (wingToggle) {
+			wing.extendPiston();
+		} else {
+			wing.retractPiston();
+		}
+	});
 }
 
-void robotInit() { 
+#endif
+
+void robotInit() {
 	deviceInit();
 	constructAuton(isLeft, isRed);
 }
